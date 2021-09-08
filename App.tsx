88ed89abb0cb07eefaf57d23c7a2f6fc8ed87490
HarrisonHenri <harrisonhenrisn@gmail.com>
2021-09-08 10:04:40 -0300

    chore: add global theme

--- App.tsx
+++ App.tsx
@@ -1,19 +1,34 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from '@expo-google-fonts/archivo'
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter'
+import AppLoading from 'expo-app-loading'
+import { useFonts } from 'expo-font'
+import { ThemeProvider } from 'styled-components/native'
+
+import { Home } from './src/Home'
+import { theme } from './src/styles/theme'
 
 export default function App(): JSX.Element {
+  const [fontsLoader] = useFonts({
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+    Inter_400Regular,
+    Inter_500Medium,
+  })
+
+  if (!fontsLoader) {
+    return <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
---
+++ src/styles/theme.ts
@@ -0,0 +1,38 @@
+export const theme = {
+  colors: {
+    additionalColors: {
+      header: '#1B1B1F',
+      background: {
+        primary: '#F4F5F6',
+        secondary: '#FFFFFF',
+      },
+      success: '#03B252',
+      shape: {
+        dark: '#E1E1E8',
+        main: '#29292E',
+      },
+    },
+    primary: {
+      main: '#DC1637',
+      light: '#FDEDEF',
+    },
+  },
+  fonts: {
+    colors: {
+      text: '#7A7A80',
+      detail: '#AEA3B3',
+      title: '#47474D',
+      line: '#EBEBF0',
+    },
+
+    primary: {
+      '400': 'Inter_400Regular',
+      '500': 'Inter_500Medium',
+    },
+    secondary: {
+      '400': 'Archivo_400Regular',
+      '500': 'Archivo_500Medium',
+      '600': 'Archivo_600SemiBold',
+    },
+  },
+}
